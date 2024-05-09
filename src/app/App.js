7b46640a48c5aba9642e8b42746eabd2555aebcb
Oleg Ukrainski <oleg.art.motion@gmail.com>
2024-05-09 15:48:11 +0300

    first commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,31 +1,49 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "./icon1.json";
 
 function App() {
   const [isReveresed, setIsReversed] = useState(false);
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const reverse = () => {
     setIsReversed(!isReveresed);
   };
 
   return (
-    <div className="container">
+    <div className="wrapper">
       {isReveresed ? (
         <>
-          <div onClick={reverse} className="blue-container">
-            <p>יניב הגבר</p>
+          <div onClick={reverse} className="blue-container container">
+            <p>item1</p>
           </div>
-          <div onClick={reverse} className="red-container">
-            <p>red</p>
+
+          <div onClick={reverse} className="red-container container">
+            <div className="lottie-container">
+              <Lottie options={defaultOptions} />
+              <p>item2</p>
+            </div>
           </div>
         </>
       ) : (
         <>
-          <div onClick={reverse} className="red-container">
-            <p>red</p>
+          <div onClick={reverse} className="red-container container">
+            <div className="lottie-container">
+              <Lottie options={defaultOptions} />
+              <p>item2</p>
+            </div>
           </div>
-          <div onClick={reverse} className="blue-container">
-            <p>blue</p>
+          <div onClick={reverse} className="blue-container container">
+            <p>item1</p>
           </div>
         </>
       )}
